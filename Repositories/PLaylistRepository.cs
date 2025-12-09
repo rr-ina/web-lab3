f43e0d22dbe8f391263d403b4f4b45b227116984
Dariia <ryndych.dariia@gmail.com>
2025-12-09 05:27:07 +0200

    Logging added.

--- Repositories/PLaylistRepository.cs
+++ Repositories/PLaylistRepository.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore; 
+using Microsoft.EntityFrameworkCore;
 using lab3.Data;
 using lab3.Models;
 
@@ -7,40 +7,66 @@ namespace lab3.Repositories
     public class PlaylistRepository : IPlaylistRepository
     {
         private readonly AppDbContext _context;
+        private readonly ILogger<PlaylistRepository> _logger;
 
-        public PlaylistRepository(AppDbContext context)
+        public PlaylistRepository(AppDbContext context, ILogger<PlaylistRepository> logger)
         {
             _context = context;
+            _logger = logger;
         }
+
         public async Task<IEnumerable<Playlist>> GetAll()
         {
-            return await _context.Playlists.ToListAsync();
+            _logger.LogInformation("Запит на отримання всіх плейлистів.");
+
+            var playlists = await _context.Playlists.ToListAsync();
+
+            _logger.LogInformation($"Успішно отримано {playlists.Count} плейлистів.");
+
+            return playlists;
         }
 
         public async Task<Playlist?> GetById(int id)
         {
+            _logger.LogInformation($"Пошук плейлиста з ID: {id}");
             return await _context.Playlists.FindAsync(id);
         }
 
         public async Task Add(Playlist playlist)
         {
+            _logger.LogInformation($"Спроба створення плейлиста: '{playlist.Name}'");
+
             await _context.Playlists.AddAsync(playlist);
-            await _context.SaveChangesAsync(); 
+            await _context.SaveChangesAsync();
+
+            _logger.LogInformation($"Плейлист '{playlist.Name}' успішно створено. Присвоєно ID: {playlist.Id}");
         }
 
         public async Task Update(Playlist playlist)
         {
+           _logger.LogInformation($"Оновлення даних плейлиста з ID: {playlist.Id}");
+            
             _context.Playlists.Update(playlist);
             await _context.SaveChangesAsync();
+            
+            _logger.LogInformation($"Плейлист ID: {playlist.Id} успішно оновлено.");
         }
 
         public async Task Delete(int id)
         {
+           _logger.LogInformation($"Запит на видалення плейлиста з ID: {id}");
+            
             var playlist = await _context.Playlists.FindAsync(id);
             if (playlist != null)
             {
                 _context.Playlists.Remove(playlist);
                 await _context.SaveChangesAsync();
+                
+                _logger.LogInformation($"Плейлист ID: {id} було успішно видалено з бази даних.");
+            }
+            else
+            {
+                _logger.LogWarning($"Плейлист з ID {id} не знайдено.");
             }
         }
     }
--- Repositories/SongRepository.cs
+++ Repositories/SongRepository.cs
@@ -7,11 +7,14 @@ namespace lab3.Repositories
     public class SongRepository : ISongRepository
     {
         private readonly AppDbContext _context;
+        private readonly ILogger<SongRepository> _logger;
 
-        public SongRepository(AppDbContext context)
+        public SongRepository(AppDbContext context, ILogger<SongRepository> logger)
         {
             _context = context;
+            _logger = logger;
         }
+
         public async Task<IEnumerable<Song>> GetAllAsync()
         {
             return await _context.Songs.ToListAsync();
@@ -19,13 +22,34 @@ namespace lab3.Repositories
 
         public async Task<Song?> GetByIdAsync(int id)
         {
-            return await _context.Songs.FindAsync(id);
+            _logger.LogInformation($"Пошук пісні за ID: {id}");
+
+            var song = await _context.Songs.FindAsync(id);
+
+            if (song == null)
+            {
+                _logger.LogWarning($"Пісню з ID {id} не знайдено в базі даних.");
+            }
+
+            return song;
         }
 
         public async Task AddAsync(Song song)
         {
-            await _context.Songs.AddAsync(song);
-            await _context.SaveChangesAsync();
+            _logger.LogInformation($"Спроба додати пісню '{song.Title}'");
+
+            try
+            {
+                await _context.Songs.AddAsync(song);
+                await _context.SaveChangesAsync();
+                _logger.LogInformation($"Пісню успішно додано з ID: {song.Id}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Помилка при додаванні пісні '{song.Title}'.");
+
+                throw; 
+            }
         }
 
         public async Task UpdateAsync(Song song)
@@ -42,11 +66,16 @@ namespace lab3.Repositories
                 _context.Songs.Remove(song);
                 await _context.SaveChangesAsync();
             }
+            else 
+            {
+                _logger.LogWarning($"Спроба видалити неіснуючу пісню ID: {id}");
+            }
         }
 
         public async Task DeleteAll()
         {
-            await _context.Playlists.ExecuteDeleteAsync();
+            _logger.LogWarning("Виконується повне видалення пісень!");
+            await _context.Songs.ExecuteDeleteAsync();
         }
     }
 }
